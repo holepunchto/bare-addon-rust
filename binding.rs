2b2bd917ecb342a907df6ddf030cd0f63b58d7ff
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-07-09 13:31:50 +0200

    Fix unused variable warnings

--- binding.rs
+++ binding.rs
@@ -4,17 +4,12 @@ use bare_rust::{
 };
 
 #[unsafe(no_mangle)]
-pub extern "C" fn bare_addon_exports(
-    env: *mut js_env_t,
-    exports: *mut js_value_t,
-) -> *mut js_value_t {
+pub extern "C" fn bare_addon_exports(env: *mut js_env_t, _: *mut js_value_t) -> *mut js_value_t {
     let env = Env::from(env);
 
     let mut exports = Object::new(&env).unwrap();
 
-    let function = Function::new(&env, |env, info| {
-        String::new(&env, "Hello from Rust").unwrap()
-    });
+    let function = Function::new(&env, |env, _| String::new(&env, "Hello from Rust").unwrap());
 
     exports
         .set_named_property("hello", function.unwrap())
