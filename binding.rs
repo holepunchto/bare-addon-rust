4cc307877934d2dab2ba897637e60adcd8540911
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-10-27 11:00:13 +0100

    Update `bare-rust`

--- binding.rs
+++ binding.rs
@@ -1,21 +1,13 @@
-use bare_rust::{
-    ffi::{js_env_t, js_value_t},
-    Env, Function, Object, String,
-};
+use bare_rust::{bare_exports, Env, Function, Object, String};
 
-#[unsafe(no_mangle)]
-pub extern "C" fn bare_addon_exports(env: *mut js_env_t, _: *mut js_value_t) -> *mut js_value_t {
-    let env = Env::from(env);
-
-    let mut exports = Object::new(&env).unwrap();
+bare_exports!(bare_addon_exports, |env| {
+    let mut exports = Object::new(&env)?;
 
     let function = Function::new(&env, |env, _| {
         Ok(String::new(&env, "Hello from Rust")?.into())
-    });
+    })?;
 
-    exports
-        .set_named_property("hello", function.unwrap())
-        .unwrap();
+    exports.set_named_property("hello", function)?;
 
-    exports.into()
-}
+    Ok(exports.into())
+});
